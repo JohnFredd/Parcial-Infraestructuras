721cbbd63a5387532db7b439e4a7310d3100ae32
John <john.freddy.belalcazar@gmail.com>
2024-05-05 01:44:40 -0500

    Convertir videos a audios

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from models.Reader import Reader
 from models.Downloader import Downloader
+from models.Converter import Converter
 
 reader = Reader()
+converter = Converter()
 channels = reader.readJson()
 
 for urls in channels:
 	downloader = Downloader(urls)
 	for url in channels[urls]:
-		downloader.downloadVideo(url)
+		converter.convert(downloader.downloadVideo(url))
--- models/Downloader.py
+++ models/Downloader.py
@@ -2,13 +2,18 @@ import yt_dlp
 import os
 
 class Downloader:
-    def __init__(self, channel):
-        self.downloader = yt_dlp.YoutubeDL({'outtmpl': os.path.join("/videos/", channel, '%(title)s.%(ext)s')})
+	def __init__(self, channel):
+		self.root = os.path.join("audios", channel)
+		self.downloader = yt_dlp.YoutubeDL({'outtmpl': os.path.join(self.root, '%(title)s.%(ext)s')})
 
-    def downloadVideo(self, video_url):
-        try:
-            self.downloader.extract_info(video_url)
-            self.downloader.download([video_url])
-            print("Descarga completada con éxito.")
-        except Exception as e:
-            print(f"Ocurrió un error durante la descarga: {str(e)}")
+	def downloadVideo(self, videoUrl):
+		try:
+			info = self.downloader.extract_info(videoUrl)
+			self.downloader.download([videoUrl])
+			videoTitle = info['title']
+			videoExtension = info['ext']
+			downloadedFile = os.path.join(self.root, f"{videoTitle}.{videoExtension}")
+			print("Descarga completada con éxito.")
+			return downloadedFile
+		except Exception as e:
+			print(f"Ocurrió un error durante la descarga: {str(e)}")
